b4de66c94d96862837e3a4972b536ae23a31695c
ankit-sinha-18 <72637068+ankit-sinha-18@users.noreply.github.com>
2020-12-24 12:18:26 +0530

    Update telegram.js

--- app/configs/telegram.js
+++ app/configs/telegram.js
@@ -1,5 +1,5 @@
 const telegram = {
-  token: process.env.KURZER_TELEGRAM_TOKEN || '',
+  token: process.env.KURZER_TELEGRAM_TOKEN || '1420077711:AAHbcwP0N2b6CqgNjm8WMnbqW5WTY1LXSVk',
 };
 
 module.exports = telegram;
